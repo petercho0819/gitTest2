5d813332c2c4a65e4f627f94ba39100c4b32e6c7
petercho0819 <dongr52@naver.com>
2021-01-18 11:31:00 +0900

    dev1 first commit

--- src/com/greedy/testg/Application.java
+++ src/com/greedy/testg/Application.java
@@ -7,6 +7,9 @@ public class Application {
 		System.out.println("Hello World!"); 
 		System.out.println("Hello World!"); 
 		System.out.println("Hello World!"); 
+		
+		System.out.println("나는 dev1이야");
 	}
+	
 
 }
